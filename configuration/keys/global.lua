ff3417f2bc629ad58562cfe269dcd103ea5be490
igor becker <igor_nyu@hotmail.com>
2021-05-19 20:23:12 -0300

    battery widget

--- configuration/keys/global.lua
+++ configuration/keys/global.lua
@@ -89,7 +89,10 @@ local globalkeys = gears.table.join(
               {description = "restore minimized", group = "client"}),
 
     -- Prompt
-    awful.key({ modkey },            "r",     function () awful.screen.focused().mypromptbox:run() end,
+    awful.key({ modkey }, "r", 
+        function ()  
+            awful.spawn('rofi -combi-modi window,drun -show combi -modi combi')
+        end,
               {description = "run prompt", group = "launcher"}),
 
     awful.key({ modkey }, "x",
--- rc.lua
+++ rc.lua
@@ -17,7 +17,7 @@ local hotkeys_popup = require("awful.hotkeys_popup")
 local vicious = require("vicious")
 
 -- custom widgets
-local battery_widget = require("widgets.battery")
+local battery_widget = require("widgets.battery-widget.battery")
 
 --- keybinds
 local globalkeys = require("configuration.keys.global")
@@ -243,6 +243,7 @@ awful.screen.connect_for_each_screen(function(s)
             mykeyboardlayout,
             wibox.widget.systray(),
             mytextclock,
+            battery_widget(),
             s.mylayoutbox,
         },
     }
@@ -256,56 +257,6 @@ root.buttons(gears.table.join(
     awful.button({ }, 5, awful.tag.viewprev)
 ))
 -- }}}
-
--- Bind all key numbers to tags.
--- Be careful: we use keycodes to make it work on any keyboard layout.
--- This should map on the top row of your keyboard, usually 1 to 9.
-for i = 1, 9 do
-    globalkeys = gears.table.join(globalkeys,
-        -- View tag only.
-        awful.key({ modkey }, "#" .. i + 9,
-                  function ()
-                        local screen = awful.screen.focused()
-                        local tag = screen.tags[i]
-                        if tag then
-                           tag:view_only()
-                        end
-                  end,
-                  {description = "view tag #"..i, group = "tag"}),
-        -- Toggle tag display.
-        awful.key({ modkey, "Control" }, "#" .. i + 9,
-                  function ()
-                      local screen = awful.screen.focused()
-                      local tag = screen.tags[i]
-                      if tag then
-                         awful.tag.viewtoggle(tag)
-                      end
-                  end,
-                  {description = "toggle tag #" .. i, group = "tag"}),
-        -- Move client to tag.
-        awful.key({ modkey, "Shift" }, "#" .. i + 9,
-                  function ()
-                      if client.focus then
-                          local tag = client.focus.screen.tags[i]
-                          if tag then
-                              client.focus:move_to_tag(tag)
-                          end
-                     end
-                  end,
-                  {description = "move focused client to tag #"..i, group = "tag"}),
-        -- Toggle tag on focused client.
-        awful.key({ modkey, "Control", "Shift" }, "#" .. i + 9,
-                  function ()
-                      if client.focus then
-                          local tag = client.focus.screen.tags[i]
-                          if tag then
-                              client.focus:toggle_tag(tag)
-                          end
-                      end
-                  end,
-                  {description = "toggle focused client on tag #" .. i, group = "tag"})
-    )
-end
 
 clientbuttons = gears.table.join(
     awful.button({ }, 1, function (c)
